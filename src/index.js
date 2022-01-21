48ca20a0323dac5fbb53a017b2d23cd0b805d755
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2022-01-20 22:23:49 -0300

    Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 /**
  * GET - Buscar uma informação dentro do servidor
  * POST - Inserir uma informação no servidor
@@ -10,15 +12,28 @@ const app = express()
  * DELETE - Deletar uma informação no servidor
  */
 
+/**
+ * Tipos de parâmetros
+ * Route Params => Identificar um recurso, editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os objetos inserção/alteração (JSON)
+ */
+
 app.get('/courses', (request, response) => {
+  const query = request.query
+  console.log(query)
   return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
 })
 
 app.post('/courses', (request, response) => {
+  const body = request.body
+  console.log(body);
   return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
 app.put('/courses/:id', (request, response) => {
+  const { id } = request.params
+  console.log(id)
   return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
